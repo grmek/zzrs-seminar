b1c97f207ae05ff585ed83d35c51e0e236e071d4
edicebokli <edi.cebokli@gmail.com>
2018-03-26 19:17:08 +0200

    algorithm1.py and algorithm2.py
    Python implementaciji Leibnizovega in Bellardovega algoritma za računanje pi.

--- algorithms/algorithm1.py
+++ algorithms/algorithm1.py
@@ -1,1 +1,13 @@
-print("3.14159", end="")
+import sys
+def main(argv):
+    n = int(argv[0])
+    pi = 0
+    sign = 1
+    for i in range(1,n,2):
+        pi = pi + sign*1/i
+        sign = sign * -1
+    pi = pi*4
+    print(pi)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
--- algorithms/algorithm2.py
+++ algorithms/algorithm2.py
@@ -1,1 +1,11 @@
-print("3.14159", end="")
+import sys
+def main(argv):
+    n = int(argv[0])
+    pi = 0
+    for i in range(0,n):
+        pi = pi + (pow(-1,i)/pow(2,10*i)) * ((-1*pow(2,5)/((4*i)+1)) + (-1/((4*i)+3)) + (pow(2,8)/((10*i)+1)) + (-1*pow(2,6)/((10*i)+3)) + (-1*pow(2,2)/((10*i)+5))  + (-1*pow(2,2)/((10*i)+7)) + (1/((10*i)+9)) )                         
+    pi = pi/pow(2,6)
+    print(pi)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
