e8fd6a9eb6415a4b0a5d8f8d16946705c447169f
edicebokli <edi.cebokli@gmail.com>
2018-03-26 20:13:46 +0200

    C PI
    C implementaciji Leibnizovega in Bellardovega algoritma za računanje pi.

--- algorithms/algorithm1.c
+++ algorithms/algorithm1.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int main()
-{
-	printf("3.14159");
-	return 0;
+int main(int argc, char *argv[]) {     
+    int n;
+    sscanf (argv[1],"%d",&n);
+    double pi = 0;
+    int sign = 1;
+    int i;
+    for(i = 1; i<=n; i=i+2){
+        pi = pi + (double)sign*1/i;
+        sign = sign * -1;
+    }
+    pi = pi*4;
+    printf("%.30f",pi);
+    return 0;
 }
+    
--- algorithms/algorithm2.c
+++ algorithms/algorithm2.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int main()
-{
-	printf("3.14159");
-	return 0;
+int main(int argc, char *argv[]) {     
+    int n;
+    sscanf (argv[1],"%d",&n);
+    double pi = 0;
+    int sign = 1;
+    int i;
+    for(i=0; i<=n; i++){
+        pi = pi + (double)((double)pow(-1,i)/(double)pow(2,10*i)) * (double)((double)(-1*pow(2,5)/(double)((4*i)+1)) + (double)(-1/(double)((4*i)+3)) + (double)(pow(2,8)/(double)((10*i)+1)) + (double)(-1*pow(2,6)/(double)((10*i)+3)) + (double)(-1*pow(2,2)/(double)((10*i)+5))  + (double)(-1*pow(2,2)/(double)((10*i)+7)) + (double)(1/(double)((10*i)+9)) );                       
+    }
+    pi = (double)pi/pow(2,6);
+    printf("%.30f",pi);
+    return 0;
 }
+    
